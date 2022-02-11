422cde2b2e5f183a5f65329286f074eb59b7caaf
Bailey Jean <baileyjleavitt@gmail.com>
2022-02-10 17:40:08 -0700

    Add stack navigation to all pages for app

--- App.js
+++ App.js
@@ -3,80 +3,204 @@ import React from 'react';
 import { StyleSheet, Text, View, Image, Button, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import * as ImagePicker from 'expo-image-picker';
 import photo from './assets/joe.png';
 
 export default function App() {
-  // STATE VARIABLES
-  const [selectedImage, setSelectedImage] = React.useState(null);
+  // VARIABLES
+  const Stack = createStackNavigator();
 
   // FUNCTIONS
-  let openImagePickerAsync = async () => {
-    let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Wishlist" component={Wishlist} />
+        <Stack.Screen name="PartnerStats" component={PartnerStats} />
+        <Stack.Screen name="DateHistory" component={DateHistory} />
+        <Stack.Screen name="GiftIdeas" component={GiftIdeas} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
-    if (permissionResult.granted === false){
-      alert("Permission to access photo is required, damnit!");
-      return;
-    }
-
-    let pickerResult = await ImagePicker.launchImageLibraryAsync();
-    
-    if (pickerResult.cancelled === true) {
-      return;
-    }
-
-    setSelectedImage({ localUri: pickerResult.uri });
-  }
-
-  if (selectedImage !== null ) {
-    return (
-      <View style={styles.container}>
+function Home({ navigation }) {
+  return (
+    <View style={styles.container}>
         <Text style={styles.header}>
-        Cupid's Quiver
+          Cupid's Quiver
         </Text>
 
-        <Image 
-          source={{ uri: selectedImage.localUri }}
-          style={styles.thumbnail}
-        />
-
         <Text style={styles.instructions}>
-          Welcome to Cupid's Quiver - the vessel to house all your romantic ideas and plans.
+          Welcome to Cupid's Quiver--your new vessel to house all your romantic ideas and plans.
         </Text>
 
         <TouchableOpacity
-          onPress={openImagePickerAsync}
+          onPress={() => navigation.navigate('Wishlist')}
           style={styles.button}>
-          <Text style={styles.buttonText}>Upload New Photo</Text>
+          <Text style={styles.buttonText}>Wishlist</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('PartnerStats')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Partner Stats</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('DateHistory')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>History</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('GiftIdeas')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Gift Ideas</Text>
         </TouchableOpacity>
 
         <StatusBar style="auto" />
       </View>
-    )
+    );
   }
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>
-        DateKeeper: The Future of Romance
-      </Text>
 
-      <Image source={photo} style={styles.photo} />
+  function Wishlist({ navigation }) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>
+            Date Wishlist
+        </Text>
+        
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Home')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Home</Text>
+        </TouchableOpacity>
 
-      <Text style={styles.instructions}>
-        Welcome to DateKeeper - the new gatekeeper for all your romantic ideas and plans.
-      </Text>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('PartnerStats')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Partner Stats</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={openImagePickerAsync}
-        style={styles.button}>
-        <Text style={styles.buttonText}>Upload New Photo</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('DateHistory')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>History</Text>
+        </TouchableOpacity>
 
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+        <TouchableOpacity
+          onPress={() => navigation.navigate('GiftIdeas')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Gift Ideas</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  function PartnerStats({ navigation }) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>
+            Partner Stats
+        </Text>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Home')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Home</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Wishlist')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Wishlist</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('DateHistory')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>History</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('GiftIdeas')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Gift Ideas</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  function DateHistory({ navigation }) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>
+            Date History
+        </Text>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Home')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Home</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Wishlist')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Wishlist</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('PartnerStats')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Partner Stats</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('GiftIdeas')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Gift Ideas</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  function GiftIdeas({ navigation }) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>
+            Gift Ideas
+        </Text>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Home')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Home</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Wishlist')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Wishlist</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('PartnerStats')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>Partner Stats</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => navigation.navigate('DateHistory')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>History</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 
 const styles = StyleSheet.create({
   container: {
@@ -102,8 +226,9 @@ const styles = StyleSheet.create({
   },
   button: { 
     backgroundColor: '#888',
-    padding: 18,
-    borderRadius: 12
+    padding: 12,
+    margin: 12,
+    borderRadius: 18
   },
   buttonText: { 
     fontSize: 20, 
